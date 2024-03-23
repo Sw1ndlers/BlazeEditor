f5c80a72f291ac4fd230fde650205f62be5ed980
Sw1ndlers <lisina2023@icloud.com>
2024-03-23 16:34:29 -0700

    Move editor to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import EditorWrapper from "@/components/EditorWrapper/EditorWrapper";
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { useCssColorHex } from "@/utils/Hooks/CssColor";
@@ -10,85 +11,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 	const headerHeight = 66;
-	const backgroundColor = useCssColorHex("base-200");
-	const contextMenuColor = useCssColorHex("base-100");
-	const contextTextColor = useCssColorHex("base-content");
 
-	const [editorContent, setEditorContent] = useState<string>("");
-	const [editorLanguage, setEditorLanguage] = useState<string>("plaintext");
-
-	const [editor, setEditor] = useState<any>(null);
-	const [editorMonaco, setEditorMonaco] = useState<any>(null);
-
-	const activeTab = useTabStore((state) => state.activeTab);
-
-	function handleEditorDidMount(editor: any, monaco: any) {
-		if (!backgroundColor || !contextMenuColor || !contextTextColor) {
-			console.error("No background color or context menu color");
-			return;
-		}
-
-		const editorRoot = document.getElementsByClassName(
-			"monaco-editor",
-		)[0]! as HTMLDivElement;
-		const editorStyle = editorRoot.style;
-
-		editorStyle.setProperty("--vscode-menu-background", contextMenuColor);
-		editorStyle.setProperty(
-			"--vscode-menu-selectionBackground",
-			backgroundColor,
-		);
-		editorStyle.setProperty("--vscode-menu-foreground", contextTextColor);
-
-		const backgroundLuminance = chroma(backgroundColor).luminance();
-		const selectionForground =
-			backgroundLuminance > 0.5
-				? chroma(backgroundColor).darken(5)
-				: chroma(backgroundColor).brighten(5);
-
-		editorStyle.setProperty(
-			"--vscode-menu-selectionForeground",
-			selectionForground.hex(),
-		);
-
-		monaco.editor.defineTheme("my-theme", {
-			base: "vs-dark",
-			inherit: true,
-			rules: [],
-			colors: {
-				"editor.background": backgroundColor,
-			},
-		});
-
-		monaco.editor.setTheme("my-theme");
-		editor.focus();
-
-		setEditor(editor);
-		setEditorMonaco(monaco);
-	}
-
-	// Resize the editor when the window resizes
-	useEffect(() => {
-		window.onresize = () => {
-			if (editor) {
-				editor.layout({});
-			}
-		};
-	}, [editor]);
-
-	// Change the value of the editor when the active tab changes
-	useEffect(() => {
-		if (!activeTab || !editor || !editorMonaco) {
-			return;
-		}
-
-		if (activeTab.validUtf == false) {
-			editor.setValue("File not displayable in editor");
-			return;
-		}
-
-		editor.setValue(activeTab.content);
-	}, [activeTab, editor, editorMonaco]);
 
 	return (
 		<div className="max-w-screen flex max-h-screen flex-col bg-base-200">
@@ -108,15 +31,7 @@ export default function Home() {
 
 				{/* Editor */}
 				<div className="-ml-3 h-full w-0.5 flex-grow pb-2 pr-1 pt-1">
-					{backgroundColor && contextMenuColor && (
-						<Editor
-							defaultLanguage="plaintext"
-							defaultValue="console.log('Best Editor')"
-							onMount={handleEditorDidMount}
-							path={activeTab?.fileElement.absolutePath}
-							// language={editorLanguage}
-						/>
-					)}
+                    <EditorWrapper />
 				</div>
 			</div>
 		</div>
---
+++ src/components/EditorWrapper/EditorWrapper.tsx
@@ -0,0 +1,113 @@
+import { useCssColorHex } from "@/utils/Hooks/CssColor";
+import { useTabStore } from "@/utils/Stores/TabStore";
+import { Editor } from "@monaco-editor/react";
+import chroma from "chroma-js";
+import { useEffect, useState } from "react";
+
+function getEditorStyle(): CSSStyleDeclaration {
+    const editorRoot = document.getElementsByClassName(
+        "monaco-editor",
+    )[0]! as HTMLDivElement;
+    return editorRoot.style;
+}
+
+
+function setEditorStyle(
+	editorStyle: CSSStyleDeclaration,
+	backgroundColor: string,
+	contextMenuColor: string,
+	contextTextColor: string,
+) {
+	editorStyle.setProperty("--vscode-menu-background", contextMenuColor);
+	editorStyle.setProperty(
+		"--vscode-menu-selectionBackground",
+		backgroundColor,
+	);
+	editorStyle.setProperty("--vscode-menu-foreground", contextTextColor);
+
+	const backgroundLuminance = chroma(backgroundColor).luminance();
+	const selectionForground =
+		backgroundLuminance > 0.5
+			? chroma(backgroundColor).darken(5)
+			: chroma(backgroundColor).brighten(5);
+
+	editorStyle.setProperty(
+		"--vscode-menu-selectionForeground",
+		selectionForground.hex(),
+	);
+}
+
+export default function EditorWrapper() {
+    const activeTab = useTabStore((state) => state.activeTab);
+
+	const backgroundColor = useCssColorHex("base-200");
+	const contextMenuColor = useCssColorHex("base-100");
+	const contextTextColor = useCssColorHex("base-content");
+
+	const [editorContent, setEditorContent] = useState<string>("");
+	const [editorLanguage, setEditorLanguage] = useState<string>("plaintext");
+
+	const [editor, setEditor] = useState<any>(null);
+	const [editorMonaco, setEditorMonaco] = useState<any>(null);
+
+	function handleEditorDidMount(editor: any, monaco: any) {
+		if (!backgroundColor || !contextMenuColor || !contextTextColor) {
+			console.error("No background color or context menu color");
+			return;
+		}
+
+        const editorStyle = getEditorStyle();
+        setEditorStyle(editorStyle, backgroundColor, contextMenuColor, contextTextColor);
+
+		monaco.editor.defineTheme("my-theme", {
+			base: "vs-dark",
+			inherit: true,
+			rules: [],
+			colors: {
+				"editor.background": backgroundColor,
+			},
+		});
+
+		monaco.editor.setTheme("my-theme");
+		editor.focus();
+
+		setEditor(editor);
+		setEditorMonaco(monaco);
+	}
+
+	// Resize the editor when the window resizes
+	useEffect(() => {
+		window.onresize = () => {
+			if (editor) {
+				editor.layout({});
+			}
+		};
+	}, [editor]);
+
+	// Change the value of the editor when the active tab changes
+	useEffect(() => {
+		if (!activeTab || !editor || !editorMonaco) {
+			return;
+		}
+
+		if (activeTab.validUtf == false) {
+			editor.setValue("File not displayable in editor");
+			return;
+		}
+
+		editor.setValue(activeTab.content);
+	}, [activeTab, editor, editorMonaco]);
+    
+	return (
+		<>
+			{backgroundColor && contextMenuColor && (
+				<Editor
+					defaultLanguage="plaintext"
+					defaultValue="console.log('Best Editor')"
+					onMount={handleEditorDidMount}
+					path={activeTab?.fileElement.absolutePath}
+				/>
+			)}
+		</>
+	);
+}
